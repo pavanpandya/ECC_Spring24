481a6258f82e8a7ad9180f9f71f340ba815ef844
Pavan Pandya <pavanpandya1311@gmail.com>
2024-03-09 22:51:23 -0500

    Typo Correction

--- run_all_examples_jobs.sh
+++ run_all_examples_jobs.sh
@@ -11,7 +11,7 @@ OUTPUT_DIR3="/Outputs/Part_02/Wordcount"
 # Job 1: Grep
 # Ask the user for word to search
 read -p "Enter word or pattern you want to search: " pattern
-hadoop jar /$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar -file /home/hadoop/Assignment_01/Part_02/Examples/Grep/mapper.py -mapper "/usr/bin/python3 /home/hadoop/Assignment_01/Part_02/Examples/Grep/mapper.py $word" -file /home/hadoop/Assignment_01/Part_02/Examples/Grep/reducer.py -reducer "/usr/bin/python3 /home/hadoop/Assignment_01/Part_02/Examples/Grep/reducer.py" -input /Input/access.log -output "$OUTPUT_DIR1" &
+hadoop jar /$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar -file /home/hadoop/Assignment_01/Part_02/Examples/Grep/mapper.py -mapper "/usr/bin/python3 /home/hadoop/Assignment_01/Part_02/Examples/Grep/mapper.py $pattern" -file /home/hadoop/Assignment_01/Part_02/Examples/Grep/reducer.py -reducer "/usr/bin/python3 /home/hadoop/Assignment_01/Part_02/Examples/Grep/reducer.py" -input /Input/access.log -output "$OUTPUT_DIR1" &
 
 echo "-----------------------------------------------------------------------------------------------------------"
 
